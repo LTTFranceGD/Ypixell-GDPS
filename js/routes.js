ede3455464f3621e418ef94d0bf99fb750a9334c
LTTFranceGD <lttdash2025@gmail.com>
2025-10-28 13:49:18 +0100

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
+import Pemonlist from './pages/Pemonlist.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
+    { path: '/pemonlist', component: Pemonlist },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
