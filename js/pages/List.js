5723d3ecf53973c3b302528fac98d3be59fcbf58
LTTFranceGD <lttdash2025@gmail.com>
2026-04-21 17:13:12 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -119,10 +119,10 @@ export default {
                     </p> 
                     <h3>Demonlist Update</h3>
                     <p>
-                         Version 2.1.0 - 21/04/2026
+                         Version 2.1.1 - 21/04/2026
                     </p>
                     <p>
-                       Spam Circles (Unnerfed) a été ajouté dans la Demonlist
+                       2 demons ont été ajoutés dans la Demonlist
                     </p>
                     <p>
                        o2 space passe de Insane Demon à Extreme Demon
